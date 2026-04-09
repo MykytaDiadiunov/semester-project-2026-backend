af29310cad61b18d2614a4c52bb7a65f694a876c
valentin <01valentin90@gmail.com>
2026-04-09 13:21:16 +0200

    feat: added Product and ProductCategory models with admin config

--- product/models/product.py
+++ product/models/product.py
@@ -1,9 +1,32 @@
 from django.db import models
 
 
-class Product(models.Model):
-    pass
-
-
 class ProductCategory(models.Model):
-    pass
+        name = models.CharField(max_length=255)
+
+        class Meta:
+            db_table = 'item_category'
+
+        def __str__(self):
+            return self.name   
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.IntegerField()
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.CASCADE, 
+        related_name="products"
+    )
+    class Meta:
+        db_table = 'item'
+        
+    def __str__(self):
+        return self.name     
+     
+
+
+
+
