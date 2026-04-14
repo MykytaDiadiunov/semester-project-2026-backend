759aaaf6b26cb335528e107db1abf72ec73e0f00
valentin <01valentin90@gmail.com>
2026-04-14 16:29:27 +0200

    feat(cart): add Cart and CartItem models with relations

--- cart/models/cart.py
+++ cart/models/cart.py
@@ -1,9 +1,36 @@
 from django.db import models
+from django.conf import settings 
 
 
 class Cart(models.Model):
-    pass
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    ) 
+    status = models.CharField(
+        max_length=50,
+         default="active"
+    )
+    items = models.ManyToManyField(
+        'product.Product',
+        through='cart.CartItem',
+        related_name="carts"
+    )
+    def __str__(self):
+        return f"Cart {self.id} for {self.user}"
 
 
 class CartItem(models.Model):
-    pass
+    cart = models.ForeignKey(
+        'cart.Cart',
+        on_delete=models.CASCADE,
+        related_name='cart_items'
+    )
+    product = models.ForeignKey(
+        'product.Product',
+        on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Cart {self.cart.id}"
