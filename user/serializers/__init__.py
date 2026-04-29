17a630a9c8d29eb94d3b92223c99480fec116a10
valentin <01valentin90@gmail.com>
2026-04-29 15:01:48 +0200

    dzik

--- user/serializers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .user import UserSerializer, CompactUserSerializer
-from .auth import RegisterUserSerializer
-
-
-__all__ = [
-    "UserSerializer",
-    "CompactUserSerializer",
-    "RegisterUserSerializer",
-]
