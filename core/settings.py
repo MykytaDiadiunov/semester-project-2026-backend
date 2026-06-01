ff2d9a8ec7844985e6ba18484fcc15fd8bc22b28
Mykyta <myk23ta@gmail.com>
2026-06-01 22:05:50 +0200

    refactor: endpoint name

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,6 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
-
 SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env.bool("DEBUG")
